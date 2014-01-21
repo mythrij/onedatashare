a76e8726d7a7ae27433baec04012046571fb626b
Brandon Ross <bwross@buffalo.edu>
2014-01-20 22:06:31 -0500

    Added any/all/and/or bell methods.

--- stork/util/Bell.java
+++ stork/util/Bell.java
@@ -10,42 +10,62 @@ public class Bell<T> implements Future<T> {
   private T object;
   private Throwable error;
   private boolean done = false;
+  private Set<Bell<? super T>> promises = Collections.emptySet();
 
   // Ring the bell with the given object and wake up any waiting threads.
-  public final synchronized Bell<T> ring(T object) {
+  public final Bell<T> ring(T object) {
     return ring(object, null);
   }
 
   // Ring the bell with null and wake up any waiting threads.
-  public final synchronized Bell<T> ring() {
+  public final Bell<T> ring() {
     return ring(null, null);
   }
 
   // Ring the bell with the given error and wake up any waiting threads.
-  public final synchronized Bell<T> ring(Throwable error) {
+  public final Bell<T> ring(Throwable error) {
     return ring(null, (error != null) ? error : new NullPointerException());
   }
 
-  // Used by the other ring methods. Only call from a synchronized context.
+  // Used by the other ring methods.
   private Bell<T> ring(T object, Throwable error) {
-    if (!done) {
+    Set<Bell<? super T>> proms;
+
+    synchronized (this) {
+      if (done)
+        return this;
       done = true;
-      if (error != null) try {
-        fail(error);
-      } catch (Throwable t) {
-        // Discard.
-      } else try {
-        done(object);
-      } catch (Throwable t) {
-        // Discard.
-      } try {
-        always();
-      } catch (Throwable t) {
-        // Discard.
-      } finally {
-        notifyAll();
-      }
-    } return this;
+      this.object = object;
+      this.error  = error;
+      proms = promises;
+      promises = null;  // Take the set away so promise() blocks.
+    }
+
+    // Call the handlers.
+    if (error != null) try {
+      fail(error);
+    } catch (Throwable t) {
+      // Discard.
+    } else try {
+      done(object);
+    } catch (Throwable t) {
+      // Discard.
+    } try {
+      always();
+    } catch (Throwable t) {
+      // Discard.
+    }
+
+    // Pass along to all the promises, then put an empty set back.
+    for (Bell<? super T> b : proms)
+      b.ring(object, error);
+    promises = Collections.emptySet();
+
+    synchronized (this) {
+      notifyAll();
+    }
+
+    return this;
   }
 
   // Cancel the bell, resolving it with a cancellation error. Returns true if
@@ -129,5 +149,167 @@ public class Bell<T> implements Future<T> {
     fail();
   } protected void always() throws Throwable {
     // This will always be run after either done() or fail().
+  }
+
+  // Promise to ring another bell when this bell rings. The order promised
+  // bells will be rung is not specified. However, promised bells are
+  // guaranteed to be rung after this bell's handlers have been called.
+  public synchronized Bell<? super T> promise(Bell<? super T> bell) {
+    if (bell.isDone()) {
+      return bell;  // Don't be silly...
+    } while (promises == null) try {
+      wait();  // We're in the process of ringing...
+    } catch (InterruptedException e) {
+      // Keep going...
+    } if (done) {
+      bell.ring(object, error);  // We've already rung, pass it on.
+    } else if (!promises.contains(bell)) switch (promises.size()) {
+      case 0:  // This is an immutable empty set. Change to singleton.
+        promises = (Set<Bell<? super T>>)(Object) Collections.singleton(bell);
+        break;
+      case 1:  // This is the singleton. Make a mutable set.
+        promises = new HashSet<Bell<? super T>>(promises);
+      default:
+        promises.add(bell);
+    } return bell;
+  }
+
+  // Convenience method for creating a bell which rings when any of the given
+  // bells rings successfully, or fails if they all fail.
+  public static <V> Bell<V> any(Bell<V>... bells) {
+    return any(Arrays.asList(bells));
+  } public static <V> Bell<V> any(Collection<Bell<V>> bells) {
+    final Set<Bell<V>> set = new HashSet<Bell<V>>(bells);
+    final Bell<V> bell = new Bell<V>();
+    if (bells.isEmpty()) {
+      return bell.ring();
+    } for (final Bell<V> b : bells) {
+      b.promise(new Bell<V>() {
+        protected void done(V v) {
+          bell.ring(v);
+        } protected void fail(Throwable t) {
+          if (bell.isDone())
+            return;
+          set.remove(b);
+          if (set.isEmpty())
+            bell.ring(t);
+        }
+      });
+      if (bell.isDone())
+        break;
+    } return bell;
+  }
+
+  // Convenience method for creating a bell which rings when all of the given
+  // bells rings successfully, or fails if any fail.
+  public static <V> Bell<V> all(Bell<V>... bells) {
+    return all(Arrays.asList(bells));
+  } public static <V> Bell<V> all(Collection<Bell<V>> bells) {
+    final Set<Bell<V>> set = new HashSet<Bell<V>>(bells);
+    final Bell<V> bell = new Bell<V>();
+    if (bells.isEmpty()) {
+      return bell.ring();
+    } for (final Bell<V> b : bells) {
+      b.promise(new Bell<V>() {
+        protected void done(V v) {
+          if (bell.isDone())
+            return;
+          set.remove(b);
+          if (set.isEmpty())
+            bell.ring(v);
+        } protected void fail(Throwable t) {
+          bell.ring(t);
+        }
+      });
+      if (bell.isDone())
+        break;
+    } return bell;
+  }
+
+  // Convenience method for boolean bells that rings with true if any of them
+  // ring with true, or false if all of them ring with false. A failed bell
+  // is considered to have rung with false.
+  public static Bell<Boolean> or(Bell<Boolean>... bells) {
+    return or(Arrays.asList(bells));
+  } public static Bell<Boolean> or(Collection<Bell<Boolean>> bells) {
+    final Set<Bell<Boolean>> set = new HashSet<Bell<Boolean>>(bells);
+    final Bell<Boolean> bell = new Bell<Boolean>();
+    if (bells.isEmpty()) {
+      return bell.ring(true);
+    } for (final Bell<Boolean> b : bells) {
+        b.promise(new Bell<Boolean>() {
+        protected void done(Boolean v) {
+          if (v)
+            bell.ring(true);
+        } protected void fail(Throwable t) {
+          if (bell.isDone())
+            return;
+          set.remove(b);
+          if (set.isEmpty())
+            bell.ring(false);
+        }
+      });
+      if (bell.isDone())
+        break;
+    } return bell;
+  }
+
+  // Convenience method for boolean bells that rings with true if all of them
+  // ring with true, or false if any of them ring with false. A failed bell
+  // is considered to have rung with false.
+  public static Bell<Boolean> and(Bell<Boolean>... bells) {
+    return and(Arrays.asList(bells));
+  } public static Bell<Boolean> and(Collection<Bell<Boolean>> bells) {
+    if (bells.isEmpty())
+      return new Bell<Boolean>().ring(true);
+    final Set<Bell<Boolean>> set = new HashSet<Bell<Boolean>>(bells);
+    final Bell<Boolean> bell = new Bell<Boolean>();
+      return bell.ring(true);
+    } for (final Bell<Boolean> b : bells) {
+      b.promise(new Bell<Boolean>() {
+        protected void done(Boolean v) {
+          if (bell.isDone()) {
+            return;
+          } if (!v) {
+            bell.ring(false);
+          } else {
+            set.remove(b);
+            if (set.isEmpty())
+              bell.ring(true);
+          }
+        } protected void fail(Throwable t) {
+          bell.ring(false);
+        }
+      });
+      if (bell.isDone())
+        break;
+    } return bell;
+  }
+
+  // Helper method used by the above methods to create "multi-bells".
+  private static <V> multiBell(Class<Bell<V>> c, Collection<Bell<V>> bells) {
+    final Set<Bell<Boolean>> set = new HashSet<Bell<Boolean>>(bells);
+    final Bell<Boolean> bell = new Bell<Boolean>();
+    if (bells.isEmpty()) {
+      return bell.ring(true);
+    } for (final Bell<Boolean> b : bells) {
+      b.promise(new Bell<Boolean>() {
+        protected void done(Boolean v) {
+          if (bell.isDone()) {
+            return;
+          } if (!v) {
+            bell.ring(false);
+          } else {
+            set.remove(b);
+            if (set.isEmpty())
+              bell.ring(true);
+          }
+        } protected void fail(Throwable t) {
+          bell.ring(false);
+        }
+      });
+      if (bell.isDone())
+        break;
+    } return bell;
   }
 }
