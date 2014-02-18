07c73b8c7e21feeff550caedbd856a80046bcedb
Brandon Ross <bwross@buffalo.edu>
2014-02-17 21:36:28 -0500

    [BROKEN] Added package info and util package.

---
+++ stork/feather/util/LocalSession.java
@@ -0,0 +1,54 @@
+package stork.feather.util;
+
+import java.io.*;
+import stork.feather.*;
+
+/**
+ * A "connection" to the local file system, used for testing Feather
+ * implementations.
+ */
+public abstract class LocalSession extends Session {
+  private final File root;
+
+  /** Create a local session with a root relative to the given path. */
+  public Session(String u) {
+    super(URI.create("file:"+u));
+    root = new File(u);
+  }
+
+  /** Get a directory listing of a path from the session. */
+  public Bell<Stat> stat(String path) {
+    File f = new File(root, path);
+    
+  }
+
+  // Create a directory at the end-point, as well as any parent directories.
+  public abstract Bell<Void> mkdir(String path);
+
+  // Remove a file or directory.
+  public abstract Bell<Void> rm(String path);
+
+  // Close the session and free any resources. This should return immediately,
+  // disallowing any further interaction, and begin the closing procedure
+  // asynchronously. The cleanup should try to happen as quickly and quietly as
+  // possible.
+  public abstract void close();
+
+  // Create an identical session with the same settings.
+  //public abstract Session duplicate();
+
+  // Return a tap to the resource returned by selecting the root URI.
+  public Tap tap() {
+    return select(uri).tap();
+  }
+
+  // Return a sink to the resource returned by selecting the root URI.
+  public Sink sink() {
+    return select(uri).sink();
+  }
+
+  // Return a resource object representing the referent of the given URI.
+  public final Resource select(String path) {
+    return select(URI.create(path));
+  } public abstract Resource select(URI uri);
+}
---
+++ stork/feather/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Utility classes which may be used by Feather implementations. The usage of
+ * these utilities is not necessary, but may be helpful for developing and
+ * testing Feather implementations.
+ */
+package stork.feather.util;
