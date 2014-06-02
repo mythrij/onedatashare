07a6bd207e3783e2f5a68719d38ec4d89de5845b
Brandon Ross <bwross@buffalo.edu>
2014-06-02 19:56:56 -0400

    Updated Module class, removed ExternalModule.

--- stork/module/ftp/FTPListCommand.java
+++ stork/module/ftp/FTPListCommand.java
@@ -4,16 +4,18 @@ package stork.module.ftp;
  * FTP listing commands in order of priority.
  */
 public enum FTPListCommand {
-  MLSC, STAT, MLSD(true), LIST(true), NLST(true);
+  MLSC(true, true),
+  STAT(true, true),
+  MLSD(false, true),
+  MLST(false, false),
+  LIST(false, true),
+  NLST(false, true);
 
-  private boolean dataChannel;
+  private boolean cc, lists;
 
-  private FTPListCommand() {
-    this(false);
-  }
-
-  private FTPListCommand(boolean dataChannel) {
-    this.dataChannel = dataChannel;
+  private FTPListCommand(boolean cc, boolean lists) {
+    this.cc = cc;
+    this.lists = lists;
   }
 
   /**
@@ -25,10 +27,13 @@ public enum FTPListCommand {
     return (n < values().length) ? values()[n] : null;
   }
 
-  /**
-   * Check if this listing command requires a data channel.
-   */
+  /** Check if this listing command requires a data channel. */
   public boolean requiresDataChannel() {
-    return dataChannel;
+    return !cc;
+  }
+
+  /** Check if the command can list subresource names. */
+  public boolean canList() {
+    return lists;
   }
 }
--- stork/module/ftp/FTPModule.java
+++ stork/module/ftp/FTPModule.java
@@ -3,16 +3,15 @@ package stork.module.ftp;
 import stork.feather.*;
 import stork.module.*;
 
-public class FTPModule extends Module<FTPSession, FTPResource> {
-  public FTPModule() {
-    super("Stork FTP Module", "ftp", "gsiftp", "gridftp");
-    version = "1.0";
-    author  = "Brandon Ross";
-    email   = "bwross@buffalo.edu";
-    description =
-      "A module for interacting with FTP systems and derivatives thereof. "+
-      "Supports RFC 2228 security extensions with GSSAPI, as well as a few "+
-      "GridFTP extensions.";
+public class FTPModule extends Module<FTPResource> {
+  {
+    name("Stork FTP Module");
+    protocols("ftp", "gsiftp", "gridftp");
+    description(
+      "A module for interacting with FTP systems and derivatives thereof.",
+      "Supports RFC 2228 security extensions with GSSAPI, as well as a few",
+      "GridFTP extensions."
+    );
   }
 
   public FTPResource select(URI uri, Credential credential) {
