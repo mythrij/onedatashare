4d1822a05b81c8924facde06c121d30a85ffbae4
Brandon Ross <bwross@buffalo.edu>
2014-11-15 23:01:21 -0500

    Added support for HEAD requests in HTTP module.

--- stork/module/http/HTTPSession.java
+++ stork/module/http/HTTPSession.java
@@ -15,7 +15,7 @@ import stork.feather.util.*;
 public class HTTPSession extends Session<HTTPSession, HTTPResource> {
 
   protected EventLoopGroup workGroup;
-  protected HTTPBuilder builder;    
+  protected HTTPBuilder builder;
 
   /**
    * A constructor of {@code HTTPSession} with a domain described
@@ -28,7 +28,7 @@ public class HTTPSession extends Session<HTTPSession, HTTPResource> {
     workGroup = new NioEventLoopGroup();
   }
 
-  public HTTPResource select(Path path) {   
+  public HTTPResource select(Path path) {
     HTTPResource resource = new HTTPResource(this, path);
 
     return resource;
@@ -54,63 +54,5 @@ public class HTTPSession extends Session<HTTPSession, HTTPResource> {
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
-  }
-
-  public static void main(String[] args) throws InterruptedException, ExecutionException {
-    //URI u = URI.create("http://www.indeed.com");  // Test 'keep-alive' connection
-    Path p1 = Path.create("l-Rochester,-NY-jobs.html");
-    Path p2 = Path.create("l-Buffalo,-NY-jobs.html");
-    //URI u = URI.create("http://www.nytimes.com"); // Test 'close' connection
-    //URI u = URI.create("http://bing.co"); // Test host 'moved' fault
-    //URI u = URI.create("http://www.microsoft.com"); // Test path 'moved' fault
-    Path p3 = Path.create("pages/national/index.html");
-    Path p4 = Path.create("pages/nyregion/index.html");
-    URI u = URI.create("https://www.dropbox.com/");
-    Path p5 = Path.create("");
-    HTTPSession s = new HTTPSession(u).initialize().get();
-    /*
-       HTTPResource r = s.select("l-Buffalo,-NY-jobs.html");
-       r.tap().start().get();
-    //r.tap().pause();
-    //Thread.sleep(1000);
-    //r.tap().resume();
-    s.select(p1).tap().start();
-    s.select(p2).tap().start();
-    s.select(p1).tap().start();
-    s.select(p2).tap().start();
-    s.select(p1).tap().start();*/
-
-    // FTP demo
-    //FTPResource dest = new FTPModule().select(
-    //  URI.create("ftp://didclab-ws8.cse.buffalo.edu/index.html")
-    //);
-    //dest.initialize().sync();
-    //s.select(p3).tap().attach(dest.sink()).tap().start().sync();
-/*
-    HTTPResource r1 = s.select(p3);
-    HTTPResource r2 = s.select(p4);
-    Bell<Stat> b1 = r1.stat();
-    Bell<Stat> b2 = r2.stat();
-    r1.tap().attach(new HexDumpResource().sink()).tap().start().sync();
-    System.out.println(b1.get().path() + " size- " + b1.get().size() + " time- " + b1.get().time);
-    r2.tap().attach(new HexDumpResource().sink()).tap().start();        
-    System.out.println(b2.get().path() + " size- " + b2.get().size() + " time- " + b2.get().time);
-
-    s.cleanup();
-    s.select(p5).tap().attach(new HexDumpResource().sink()).tap().start();
-    s.select(p5).tap().attach(new HexDumpResource().sink()).tap().start();
-    s.select(p5).tap().attach(new HexDumpResource().sink()).tap().start();
-    s.select(p5).tap().attach(new HexDumpResource().sink()).tap().start();
-
-    Thread.sleep(3000);
-    System.out.println(
-        s.builder.channel.isActive() + " " +
-        s.builder.channel.isInputShutdown() + " " +
-        s.builder.channel.isOutputShutdown() + " " +
-        s.builder.channel.isOpen() + " " +
-        s.builder.channel.toString());
-    s.workGroup.shutdownGracefully();*/
-    
-    s.select(p5).tap().attach(new HexDumpResource().sink()).tap().start();
   }
 }
